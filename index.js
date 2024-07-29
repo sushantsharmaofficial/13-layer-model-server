da4a1f1d236b1e7bb6f5e948eff8ebcf76d600b6
Sushant Sharma <sushant.ku.sh.70@gmail.com>
2024-07-29 13:36:05 +0545

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 // Middleware
@@ -60,9 +60,6 @@ app.get("/interactions", async (req, res) => {
   }
 });
 
-app.use(express.json());
-app.use(cors());
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
